4bac849455efb72fe2adaeec5c8e89f2a71f9c64
Francisco Machuca <machuxt10@gmail.com>
2022-03-14 23:19:29 -0300

    Updates

---
+++ assets/index.1c4fbc6b.js
@@ -0,0 +1,169 @@
+var D=Object.defineProperty,L=Object.defineProperties;var T=Object.getOwnPropertyDescriptors;var R=Object.getOwnPropertySymbols;var E=Object.prototype.hasOwnProperty,F=Object.prototype.propertyIsEnumerable;var M=(t,n,l)=>n in t?D(t,n,{enumerable:!0,configurable:!0,writable:!0,value:l}):t[n]=l,a=(t,n)=>{for(var l in n||(n={}))E.call(n,l)&&M(t,l,n[l]);if(R)for(var l of R(n))F.call(n,l)&&M(t,l,n[l]);return t},r=(t,n)=>L(t,T(n));import{s as x,j as o,B as d,a as e,T as h,S as O,b as z,c as q,d as U,e as J,f as W,g as H,h as G,i as K,F as Y,k as Q,l as V,m as Z,r as v,n as $,o as X,R as P,M as ee,N as C,p as te,q as S,t as ne,u as ie}from"./vendor.23465f19.js";const oe=function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))f(i);new MutationObserver(i=>{for(const s of i)if(s.type==="childList")for(const u of s.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&f(u)}).observe(document,{childList:!0,subtree:!0});function l(i){const s={};return i.integrity&&(s.integrity=i.integrity),i.referrerpolicy&&(s.referrerPolicy=i.referrerpolicy),i.crossorigin==="use-credentials"?s.credentials="include":i.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function f(i){if(i.ep)return;i.ep=!0;const s=l(i);fetch(i.href,s)}};oe();const ae="_content_10cbt_2",re="_content__container_10cbt_15",se="_opacity_10cbt_1",le="_content__container__text_10cbt_43",ce="_content__container__list_10cbt_48",de="_change_10cbt_1",he="_content__container__list__item_10cbt_60";var g={content:ae,content__container:re,opacity:se,content__container__text:le,content__container__list:ce,change:de,content__container__list__item:he},pe="/assets/certificate.441018c9.png",me="/assets/machux.c0c97f5f.jpg";const c={width:"60px",height:"60px"},ue=()=>o(d,{children:[e("div",{className:g.content,children:o("div",{className:g.content__container,children:[e("p",{className:g.content__container__text,children:"Hello"}),o("ul",{className:g.content__container__list,children:[e("li",{className:g.content__container__list__item,children:"World!"}),e("li",{className:g.content__container__list__item,children:"Recruiters!"}),e("li",{className:g.content__container__list__item,children:"Clients!"}),e("li",{className:g.content__container__list__item,children:"Everybody!"})]})]})}),o(d,{sx:{position:"absolute",top:{xs:"42%",md:"46%"},margin:"0 auto",textAlign:"center"},children:[e("span",{style:{fontSize:"25px"},children:"I am Francisco Machuca, a Full Stack Developer specialized in React and Node.js"}),e("h1",{children:"Technologies:"}),o(d,{sx:{width:"100vw",m:{xs:1,md:0},display:"flex",flexWrap:"wrap",justifyContent:"space-around"},children:[e(h,{title:"Git",children:e("div",{children:e(O,{style:r(a({},c),{color:"orangered"})})})}),e(h,{title:"GitHub",children:e("div",{children:e(z,{style:r(a({},c),{color:"white"})})})}),e(h,{title:"HTML5",children:e("div",{children:e(q,{style:r(a({},c),{color:"orange"})})})}),e(h,{title:"CSS3",children:e("div",{children:e(U,{style:r(a({},c),{color:"#006fb4"})})})}),e(h,{title:"JavaScript",children:e("div",{children:e(J,{style:r(a({},c),{color:"yellow"})})})}),e(h,{title:"React",children:e("div",{children:e(W,{style:r(a({},c),{color:"#61dbfb"})})})}),e(h,{title:"Redux",children:e("div",{children:e(H,{style:r(a({},c),{color:"violet"})})})}),e(h,{title:"Material UI",children:e("div",{children:e(G,{style:r(a({},c),{color:"blue"})})})}),e(h,{title:"Styled Components",children:e("div",{children:e(K,{style:r(a({},c),{color:"pink"})})})}),e(h,{title:"NodeJS",children:e("div",{children:e(Y,{style:r(a({},c),{color:"darkgreen"})})})}),e(h,{title:"Express",children:e("div",{children:e(Q,{style:r(a({},c),{color:"white"})})})}),e(h,{title:"Sequelize",children:e("div",{children:e(V,{style:r(a({},c),{color:"blue"})})})}),e(h,{title:"PostgreSQL",children:e("div",{children:e(Z,{style:r(a({},c),{color:"lightblue"})})})})]}),e(ge,{href:"../FranciscoMachucaResume.pdf",target:"_blank",children:"Download CV"}),o(d,{sx:{mt:4},children:[e("h1",{children:"About Me:"}),e(d,{sx:{width:"200px",height:"200px",overflow:"hidden",margin:"0 auto"},children:e(h,{title:"This is me at Nahuel Huapi lake, Bariloche, Argentina",children:e("img",{style:{width:"200px",height:"200px",margin:"0 auto",borderRadius:"100px"},src:me,alt:"Francisco"})})}),e(d,{sx:{backgroundColor:"#15151d",fontSize:"25px",p:2,width:{xs:"90vw",md:"79vw"},margin:"20px auto",borderRadius:"7px"},children:e("p",{style:{fontStyle:"italic",textAlign:"justify"},children:`"Well, where to start... I'm 21 years old and I'm from Argentina. My beginnings in programming were thanks to the movie The Social Network. I was interested in how a single student with a great idea and a computer created a social network that allowed us to communicate from anywhere in the world. It inspired me so much that I decided to quit my medical studies and start studying computer science at university. I am now in my second year and five months ago I finished the Soy Henry programming bootcamp. Thanks to my perseverance and discipline I am now a Full Stack Web developer and I am on my way to find my great idea to contribute to the world."`})}),e("p",{style:{fontSize:"25px"},children:"I show you my certificate of which I am very proud:"}),e(d,{sx:{width:{xs:"80vw",md:"60vw"},margin:"10px auto"},children:e("img",{style:{width:"100%",height:"100%"},src:pe,alt:"Certificate"})})]})]})]}),ge=x("a")`
+  margin: 12px auto;
+  text-decoration: none;
+  display: block;
+  background-color: #357ded;
+  color: white;
+  width: 150px;
+  border-radius: 7px;
+  height: 50px;
+  line-height: 50px;
+  &:hover{
+    background-color: lightblue;
+  }
+`,xe=new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);function A(t){let n={};return(t.name.length<5||t.name.length>60)&&(n.name="Name must contain between 5 and 60 characters"),xe.test(t.email)||(n.email="Wrong email"),(t.message.length<10||t.message.length>500)&&(n.message="Message must contain between 10 and 300 characters"),n}const k={name:"",email:"",message:""},fe=()=>{const[t,n]=v.exports.useState(k),[l,f]=v.exports.useState(!1),[i,s]=v.exports.useState(k),[u,_]=v.exports.useState(null),b=p=>{const{name:m,value:B}=p.target;n(r(a({},t),{[m]:B}))},y=p=>{b(p),s(A(t))},w=p=>{b(p),s(A(t))},j=p=>{p.preventDefault(),f(!0),fetch("https://formsubmit.co/ajax/ebc7d088d5e53ab272f07a91389e03fa",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json"},body:JSON.stringify(t)}).then(m=>m.json()).then(m=>{f(!1),_(m),setTimeout(()=>{_(null)},5e3)}).catch(m=>console.log(m)),n(k),s(k)};return o(d,{children:[e("h3",{children:"Contact me via:"}),o(d,{sx:{display:"flex",flexWrap:"wrap",justifyContent:"center"},children:[e("a",{href:"https://linkedin.com/in/franciscomachucafullstack/",target:"_blank",children:e($,{style:r(a({},c),{color:"white",margin:"10px"})})}),e("a",{href:"https://github.com/machux21",target:"_blank",children:e(z,{style:r(a({},c),{color:"white",margin:"10px"})})}),e("a",{href:"https://twitter.com/machux22",target:"_blank",children:e(X,{style:r(a({},c),{color:"white",margin:"10px"})})})]}),e("hr",{style:{width:"50%",color:"white"}}),e("h3",{children:"Or send me a message:"}),e(d,{className:"animate__animated animate__fadeInUp",sx:{width:{xs:"90vw",md:"50vw"},margin:"0 auto",mt:{xs:2,md:4}},children:o("form",{onSubmit:j,style:{width:"100%",display:"flex",flexDirection:"column"},children:[e(I,{type:"text",name:"name",placeholder:"Name",value:t.name,onChange:b,onKeyUp:w,onBlur:y,required:!0}),i.name&&e(N,{children:i.name}),e(I,{type:"email",name:"email",placeholder:"Email",value:t.email,onChange:b,onKeyUp:w,onBlur:y,required:!0}),i.email&&e(N,{children:i.email}),e(_e,{cols:"30",rows:"10",name:"message",value:t.message,placeholder:"Write your message",onChange:b,onBlur:y,onKeyUp:w,required:!0}),i.message&&e(N,{children:i.message}),e(ye,{disabled:Object.entries(i).length!==0,type:"submit",children:l?"Loading...":"Send"})]})}),u?e(be,{children:u.message}):null]})},be=x("div")`
+	position: fixed;
+	bottom: 2%;
+	right: 1%;
+	background-color: #33bb5e;
+	color: white;
+	padding: 10px;
+	border-radius: 8px;
+	line-height: 60px;
+	text-align: center;
+	font-size: 20px;
+`,N=x("p")`
+	background-color: #e62020;
+	color: white;
+	font-size: 20px;
+	border-radius: 10px;
+	padding: 4px;
+	margin: 5px 0 5px 0;
+`,I=x("input")`
+	margin: 0;
+	margin-top: 8px;
+	height: 50px;
+	font-size: 18px;
+	border: none;
+	border-radius: 10px;
+	padding-left: 20px;
+	border: none;
+	&:focus {
+		outline: none;
+		border: 3px solid lightblue;
+	}
+`,ye=x("button")`
+	width: 100%;
+	height: 50px;
+	margin: 10px 0 10px 0;
+	border: none;
+	border-radius: 7px;
+	background-color: #357ded;
+	color: white;
+	transition: 0.2s;
+	&:hover {
+		background-color: #5a92ed;
+		color: #192a45;
+		cursor: pointer;
+	}
+	&:disabled {
+		background-color: lightblue;
+		color: grey;
+		cursor: auto;
+	}
+`,_e=x("textarea")`
+	font-family: 'Roboto Mono', monospace;
+	border-radius: 10px;
+	border: none;
+	resize: none;
+	padding: 20px;
+	margin-top: 10px;
+	font-family: arial;
+	font-size: 16px;
+	&:focus {
+		outline: none;
+		border: 3px solid lightblue;
+	}
+`,we={position:"absolute",top:"50%",left:"50%",transform:"translate(-50%, -50%)",width:{xs:"80vw",md:"90vw"},height:"80vh",bgcolor:"#15151d",border:"none",borderRadius:"7px",boxShadow:24,p:{xs:1,md:4},display:"flex",flexDirection:{xs:"column",md:"row"},overflow:"auto"},ve=({title:t,shortDescription:n,longDescription:l,coverPage:f,features:i,stack:s,link:u,img:_})=>{const[b,y]=P.useState(!1);return o(d,{children:[o(ke,{children:[e("img",{src:f}),o("div",{className:"info",children:[e("h1",{children:t}),e("p",{children:n}),e("button",{onClick:()=>y(!0),children:"Read More"})]})]}),e(ee,{open:b,onClose:()=>y(!1),"aria-labelledby":"modal-modal-title","aria-describedby":"modal-modal-description",children:o(d,{sx:we,children:[o(d,{sx:{width:{xs:"90%",md:"45%"},margin:{xs:"0 auto",md:"0 20px 0 0"},pr:{md:1},overflowY:{md:"scroll"}},children:[e("h1",{children:t}),e("p",{style:{textAlign:"justify",width:"100%"},children:l}),o("div",{style:{textAlign:"left"},children:[e("h2",{children:"Features"}),e("ul",{children:i.map((p,m)=>e("li",{children:p},m))})]}),o("div",{style:{textAlign:"left"},children:[e("h2",{children:"Stack"}),e("ul",{children:s.map((p,m)=>e("li",{children:p},m))})]}),o(Se,{href:u,target:"_blank",children:["Go to ",t]})]}),e(d,{sx:{width:{xs:"85%",md:"55%"},margin:"0 auto"},children:e("a",{href:u,target:"_blank",children:e("img",{style:{width:"100%",height:"100%",margin:"0 auto"},src:_,alt:t})})})]})})]})},Se=x("a")`
+  margin: 12px auto;
+  text-decoration: none;
+  display: block;
+  background-color: #357ded;
+  color: white;
+  width: auto;
+  border-radius: 7px;
+  height: 50px;
+  line-height: 50px;
+  &:hover{
+    background-color: lightblue;
+  }
+`,ke=x("div")`
+  min-width: 200px;
+  max-width: 300px;
+  height: 300px;
+  margin: 15px;
+  border-radius: 15px;
+  padding: 1.5rem;
+  background: white;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  transition: 0.4s ease-out;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
+  &:hover {
+    transform: translateY(20px);
+  }
+  &:hover:before {
+    opacity: 1;
+  }
+  &:hover .info {
+    opacity: 1;
+    transform: translateY(0px);
+    text-align: center;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 2;
+    transition: 0.5s;
+    opacity: 0;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 15px;
+  }
+  .info {
+    position: relative;
+    z-index: 3;
+    color: white;
+    opacity: 0;
+    transform: translateY(30px);
+    transition: 0.5s;
+  }
+  .info h1 {
+    margin: 0px;
+  }
+  .info p {
+    letter-spacing: 1px;
+    font-size: 15px;
+    margin-top: 8px;
+  }
+  .info button {
+    padding: 0.6rem;
+    outline: none;
+    border: none;
+    border-radius: 7px;
+    background: #357ded;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    transition: 0.4s ease;
+  }
+  .info button:hover {
+    background: lightblue;
+    color: white;
+  }
+`,Ce=[{title:"Countries App",shortDescription:"A educational Countries app",longDescription:"This is an educational web application about countries and their geography. It shows a brief description of all the countries in the world and you can add tourist activities specific to each country.",stack:["JavaScript","React","Redux","Styled Components","Node Js","Express","Sequelize","Postgres"],features:["Searching","Filtering","Pagination","Add tourist activities"],link:"https://countries-app-2022.vercel.app/",img:"https://media.istockphoto.com/photos/multi-ethnic-people-portraits-and-national-flags-picture-id510075292?k=6&m=510075292&s=612x612&w=0&h=OcuwVEPipLvMnQwc0-HbQX5QzB8NjOfBEw7l6LR9w1g=",coverPage:"https://cdn.decrypt.co/resize/1024/height/512/wp-content/uploads/2022/03/Ukraine-Flag-gID_4.png"},{title:"Mangaka",shortDescription:"A indie mangas store for readers",longDescription:"A complete mangas store where anybody can create and sell his mangas, and live of what they love. You can read mangas, and bought them with MangaCoins (an intern currency that you can change for real money)",stack:["JavaScript","React","Redux","React Router","Material UI","NodeJS","TypeScript","Express","Passport.js","Prisma","Postgres"],features:["Searching","Filtering","Pagination","Create mangas","Buy mangas","Add coments","Buy MangaCoins","Super Admin options to control the mangas content","Read mangas","The option to buy a single manga or the complete colection"],link:"https://deploy-client-kappa.vercel.app",img:"https://img.interempresas.net/fotos/2721187.png",coverPage:"https://www.japonalternativo.com/wp-content/uploads/2021/12/mejor-dibujante-manga-naruto.jpg"}],Ne=()=>e("div",{className:"animate__animated animate__fadeInUp",style:{margin:"0 auto",width:"90vw",display:"inline-flex",flexWrap:"wrap",justifyContent:"center",marginBottom:"15px"},children:Ce.map((t,n)=>e(ve,{title:t.title,longDescription:t.longDescription,shortDescription:t.shortDescription,coverPage:t.coverPage,features:t.features,stack:t.stack,link:t.link,img:t.img},n))}),je=()=>o(d,{sx:{p:{md:2},margin:"0 auto",display:"flex",flexDirection:{xs:"column",md:"row"},justifyContent:"space-between"},children:[e(C,{className:({isActive:t})=>t?"title":"title-inactive",to:"/",children:e("span",{children:"MachuxDev"})}),o(d,{sx:{display:"flex",justifyContent:"space-around",mx:2},children:[e(C,{className:({isActive:t})=>t?"active":"inactive",to:"/projects",children:e("span",{children:"Projects"})}),e(C,{className:({isActive:t})=>t?"active":"inactive",to:"/contact",children:e("span",{children:"Contact Me"})})]})]});function Re(){return o("div",{children:[e(je,{}),e("div",{className:"bg"}),e("div",{className:"bg bg2"}),e("div",{className:"bg bg3"}),e(te,{children:o(S,{path:"/",children:[e(S,{index:!0,element:e(ue,{})}),e(S,{path:"contact",element:e(fe,{})}),e(S,{path:"projects",element:e(Ne,{})})]})})]})}ne.render(e(ie,{children:e(P.StrictMode,{children:e(Re,{})})}),document.getElementById("root"));
