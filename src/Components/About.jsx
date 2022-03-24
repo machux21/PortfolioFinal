1279acd8eee524bd02468016e08bc122d81ad6a3
Francisco Machuca <machuxt10@gmail.com>
2022-03-23 22:01:19 -0300

    Responsive fixed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,12 +18,15 @@ import {
 } from "react-icons/si";
 import Certificate from "../certificate.png";
 import Machux from "../machux.jpg";
-import Resume from '../FranciscoMachucaResume.pdf'
+import Resume from "../FranciscoMachucaResume.pdf";
 import { FaNode } from "react-icons/fa";
-export const iconStyle = { width: "60px", height: "60px" };
+export const iconStyle = { width: "60px", height: "60px", margin: "10px" };
+
 const About = () => {
 	return (
-		<Box>
+		<Box sx={{position: "relative", display: "flex", justifyContent: "center", flexDirection: "column", mt: 12}}>
+		<Box sx={{position: "relative", margin: "0 auto"}}>
+			{/*ANIMATION CSS*/}
 			<div className={Styles.content}>
 				<div className={Styles.content__container}>
 					<p className={Styles.content__container__text}>Hello</p>
@@ -44,209 +47,195 @@ const About = () => {
 					</ul>
 				</div>
 			</div>
-			<Box
-				sx={{
-					position: "absolute",
-					top: { xs: "42%", md: "46%" },
-					margin: "0 auto",
-					textAlign: "center",
-				}}
-			>
-				<span style={{ fontSize: "25px", alignText: "center", margin: "0 auto" }}>
-					I am Francisco Machuca, a Full Stack Developer specialized
-					in React and Node.js
-				</span>
-				<h1>Technologies:</h1>
-				<Box
-					sx={{
-						width: "95vw",
-						margin: "0 auto"
-						display: "flex",
-						flexWrap: "wrap",
-						justifyContent: "space-around",
-						p: {xs: 2, md: 1}
+			</Box>
+			<Box>
+				<p
+					style={{
+						fontSize: "25px",
+						textAlign: "center",
+						margin: "0 auto",
+						padding: "0",
 					}}
 				>
-					<Tooltip title="Git">
-						<div>
-							<SiGit
-								style={{ ...iconStyle, color: "orangered" }}
-							/>
-						</div>
-					</Tooltip>
+					I am Francisco Machuca, a Full Stack Developer specialized
+					in React and Node.js
+				</p>
+			</Box>
+			{/*TECHNOLOGIES*/}
+			<h1>Technologies:</h1>
+			<Box
+				sx={{
+					width: "95vw",
+					margin: "0 auto",
+					display: "flex",
+					flexWrap: "wrap",
+					justifyContent: { xs: "center", md: "space-around" },
+					p: { xs: 2, md: 1 },
+				}}
+			>
+				<Tooltip title="Git">
+					<div>
+						<SiGit style={{ ...iconStyle, color: "orangered" }} />
+					</div>
+				</Tooltip>
 
-					<Tooltip title="GitHub">
-						<div>
-							<SiGithub
-								style={{ ...iconStyle, color: "white" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="HTML5">
-						<div>
-							<SiHtml5
-								style={{ ...iconStyle, color: "orange" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="CSS3">
-						<div>
-							<SiCss3
-								style={{ ...iconStyle, color: "#006fb4" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="JavaScript">
-						<div>
-							<SiJavascript
-								style={{ ...iconStyle, color: "yellow" }}
-							/>
-						</div>
-					</Tooltip>
+				<Tooltip title="GitHub">
+					<div>
+						<SiGithub style={{ ...iconStyle, color: "white" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="HTML5">
+					<div>
+						<SiHtml5 style={{ ...iconStyle, color: "orange" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="CSS3">
+					<div>
+						<SiCss3 style={{ ...iconStyle, color: "#006fb4" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="JavaScript">
+					<div>
+						<SiJavascript
+							style={{ ...iconStyle, color: "yellow" }}
+						/>
+					</div>
+				</Tooltip>
 
-					<Tooltip title="React">
-						<div>
-							<SiReact
-								style={{ ...iconStyle, color: "#61dbfb" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="Redux">
-						<div>
-							<SiRedux
-								style={{ ...iconStyle, color: "violet" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="Material UI">
-						<div>
-							<SiMaterialui
-								style={{ ...iconStyle, color: "blue" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="Styled Components">
-						<div>
-							<SiStyledcomponents
-								style={{ ...iconStyle, color: "pink" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="NodeJS">
-						<div>
-							<FaNode
-								style={{ ...iconStyle, color: "darkgreen" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="Express">
-						<div>
-							<SiExpress
-								style={{ ...iconStyle, color: "white" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="Sequelize">
-						<div>
-							<SiSequelize
-								style={{ ...iconStyle, color: "blue" }}
-							/>
-						</div>
-					</Tooltip>
-					<Tooltip title="PostgreSQL">
-						<div>
-							<SiPostgresql
-								style={{ ...iconStyle, color: "lightblue" }}
-							/>
-						</div>
-					</Tooltip>
-				</Box>
-				<LinkButton href={Resume} download>
-					Download Resume
-				</LinkButton>
-				<Box sx={{ mt: 4 }}>
-					<h1>About Me:</h1>
-					<Box
-						sx={{
+				<Tooltip title="React">
+					<div>
+						<SiReact style={{ ...iconStyle, color: "#61dbfb" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="Redux">
+					<div>
+						<SiRedux style={{ ...iconStyle, color: "violet" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="Material UI">
+					<div>
+						<SiMaterialui style={{ ...iconStyle, color: "blue" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="Styled Components">
+					<div>
+						<SiStyledcomponents
+							style={{ ...iconStyle, color: "pink" }}
+						/>
+					</div>
+				</Tooltip>
+				<Tooltip title="NodeJS">
+					<div>
+						<FaNode style={{ ...iconStyle, color: "darkgreen" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="Express">
+					<div>
+						<SiExpress style={{ ...iconStyle, color: "white" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="Sequelize">
+					<div>
+						<SiSequelize style={{ ...iconStyle, color: "blue" }} />
+					</div>
+				</Tooltip>
+				<Tooltip title="PostgreSQL">
+					<div>
+						<SiPostgresql
+							style={{ ...iconStyle, color: "lightblue" }}
+						/>
+					</div>
+				</Tooltip>
+			</Box>
+			<LinkButton href={Resume} download>
+				Download Resume
+			</LinkButton>
+			<h1>About Me:</h1>
+			<Box
+				sx={{
+					width: "200px",
+					height: "200px",
+					overflow: "hidden",
+					margin: "20px auto",
+				}}
+			>
+				<Tooltip title="This is me at Nahuel Huapi lake, Bariloche, Argentina">
+					<img
+						style={{
 							width: "200px",
 							height: "200px",
-							overflow: "hidden",
 							margin: "0 auto",
+							borderRadius: "100px",
 						}}
-					>
-						<Tooltip title="This is me at Nahuel Huapi lake, Bariloche, Argentina">
-							<img
-								style={{
-									width: "200px",
-									height: "200px",
-									margin: "0 auto",
-									borderRadius: "100px",
-								}}
-								src={Machux}
-								alt="Francisco"
-							/>
-						</Tooltip>
-					</Box>
-					<Box
-						sx={{
-							backgroundColor: "#15151d",
-							fontSize: "25px",
-							p: 2,
-							width: { xs: "90vw", md: "79vw" },
-							margin: "0 auto",
-							borderRadius: "7px",
-						}}
-					>
-						<p
-							style={{
-								fontStyle: "italic",
-								textAlign: "justify",
-							}}
-						>
-							"Well, where to start... I'm 21 years old and I'm
-							from Argentina. My beginnings in programming were
-							thanks to the movie The Social Network. I was
-							interested in how a single student with a great idea
-							and a computer created a social network that allowed
-							us to communicate from anywhere in the world. It
-							inspired me so much that I decided to quit my
-							medical studies and start studying computer science
-							at university. I am now in my second year and five
-							months ago I finished the Soy Henry programming
-							bootcamp. Thanks to my perseverance and discipline I
-							am now a Full Stack Web developer and I am on my way
-							to find my great idea to contribute to the world."
-						</p>
-					</Box>
-					<p style={{ fontSize: "25px" }}>
-						I show you my certificate of which I am very proud:
-					</p>
-					<Box
-						sx={{
-							width: { xs: "80vw", md: "60vw" },
-							margin: "10px auto",
-						}}
-					>
-						<img style={{width: "100%", height: "100%"}} src={Certificate} alt="Certificate" />
-					</Box>
-				</Box>
+						src={Machux}
+						alt="Francisco"
+					/>
+				</Tooltip>
+			</Box>
+			<Box
+				sx={{
+					backgroundColor: "#15151d",
+					fontSize: "25px",
+					p: 2,
+
+					width: { xs: "90vw", md: "79vw" },
+					margin: "0 auto",
+					borderRadius: "7px",
+				}}
+			>
+				<p
+					style={{
+						fontStyle: "italic",
+						textAlign: "justify",
+					}}
+				>
+					"Well, where to start... I'm 21 years old and I'm from
+					Argentina. My beginnings in programming were thanks to the
+					movie The Social Network. I was interested in how a single
+					student with a great idea and a computer created a social
+					network that allowed us to communicate from anywhere in the
+					world. It inspired me so much that I decided to quit my
+					medical studies and start studying computer science at
+					university. I am now in my second year and five months ago I
+					finished the Soy Henry programming bootcamp. Thanks to my
+					perseverance and discipline I am now a Full Stack Web
+					developer and I am on my way to find my great idea to
+					contribute to the world."
+				</p>
+			</Box>
+			<Box
+				sx={{
+					width: { xs: "80vw", md: "60vw" },
+					margin: "0px auto",
+					mb: 2
+				}}
+			>
+				<p style={{ fontSize: "25px" }}>
+					I show you my certificate of which I am very proud:
+				</p>
+				<img
+					style={{ width: "100%", height: "100%" }}
+					src={Certificate}
+					alt="Certificate"
+				/>
 			</Box>
 		</Box>
 	);
 };
 
 const LinkButton = styled("a")`
-  margin: 12px auto;
-  text-decoration: none;
-  display: block;
-  background-color: #357ded;
-  color: white;
-  width: 180px;
-  border-radius: 7px;
-  height: 50px;
-  line-height: 50px;
-  &:hover{
-    background-color: lightblue;
-  }
-`
+	margin: 12px auto;
+	text-decoration: none;
+	display: block;
+	background-color: #357ded;
+	color: white;
+	width: 180px;
+	border-radius: 7px;
+	height: 50px;
+	line-height: 50px;
+	&:hover {
+		background-color: lightblue;
+	}
+`;
+
 export default About;
